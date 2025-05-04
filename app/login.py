fc8375aae8d118d4efbe14c251a99a2668b498d9
Supriya M <144978458+supriyachola@users.noreply.github.com>
2025-05-04 11:24:56 +0530

    Update login.py

--- app/login.py
+++ app/login.py
@@ -6,19 +6,18 @@ import sqlite3
 
 def open_login_window():
     main_window = tk.Tk()
-    main_window.title("AI powered matching profile")
+    main_window.title("AI Powered Matching Profile")
     main_window.geometry("400x300")
-    main_window.configure(bg="#7A397B")  
+    main_window.configure(bg="#7A397B")
 
     tk.Label(main_window, text="Welcome to AI Conferencing App", font=("Arial", 16, "bold"), fg="#ffffff", bg="#7A397B").pack(pady=10)
 
-
-    tk.Label(main_window, text="Username", fg ="#ffffff", bg="#7A397B").pack()
-    login_username_entry = tk.Entry(main_window, bg ="#ffffff", fg="#000000",relief="flat", width=25)
+    tk.Label(main_window, text="Username", fg="#ffffff", bg="#7A397B").pack()
+    login_username_entry = tk.Entry(main_window, bg="#ffffff", fg="#000000", relief="flat", width=25)
     login_username_entry.pack(pady=5, ipady=3)
 
     tk.Label(main_window, text="Password", fg="#ffffff", bg="#7A397B").pack()
-    login_password_entry = tk.Entry(main_window, show="*", bg ="#ffffff", fg="#000000",relief="flat", width=25)
+    login_password_entry = tk.Entry(main_window, show="*", bg="#ffffff", fg="#000000", relief="flat", width=25)
     login_password_entry.pack(pady=5, ipady=3)
 
     def login_user():
@@ -38,11 +37,7 @@ def open_login_window():
         else:
             messagebox.showerror("Login", "Invalid username or password!")
 
-    tk.Button(main_window, text="Login", command=login_user, bg ="#C8A2C8",fg="#000000").pack(pady=5)
-    tk.Button(main_window, text="Register", command=open_registration_window,bg ="#C8A2C8",fg="#000000" ).pack(pady=5)
+    tk.Button(main_window, text="Login", command=login_user, bg="#C8A2C8", fg="#000000").pack(pady=5)
+    tk.Button(main_window, text="Register", command=open_registration_window, bg="#C8A2C8", fg="#000000").pack(pady=5)
+
     main_window.mainloop()
-
-from login import open_login_window
-
-if __name__ == "__main__":
-    open_login_window()
